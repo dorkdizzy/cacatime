9e56b5e0d78ad50f65eb1d408e6c635cef322b48
dorkdizzy <74273424+dorkdizzy@users.noreply.github.com>
2021-05-03 20:04:38 -0400

    create main go lol

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pkg/browser"
+)
+
+func main() {
+	currentTimeSecond := time.Now().Second()
+	if currentTimeSecond == 3 {
+		fmt.Println("It is caca time!!!!!")
+		browser.OpenURL("https://img.huffingtonpost.com/asset/5c93860b220000c9001c475b.jpeg")
+	} else {
+		fmt.Println("Je suis desole poto, it is not caca time :((((((")
+	}
+}
