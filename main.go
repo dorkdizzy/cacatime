16a625428bf9d80e7052a7438811ed7662570a8e
salsatequila <83643738+salsatequila@users.noreply.github.com>
2021-05-04 13:06:04 +0200

    Update main.go
    Ajout d'un message 1 seconde avant qu'il soit temps.

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 
 func main() {
 	currentTimeSecond := time.Now().Second()
-	if currentTimeSecond == 3 {
+	if currentTimeSecond == 2 {
+		fmt.Println("It is almost caca time, tiens bon !")
+	} else if currentTimeSecond == 3 {
 		fmt.Println("It is caca time!!!!!")
 		browser.OpenURL("https://img.huffingtonpost.com/asset/5c93860b220000c9001c475b.jpeg")
 	} else {
